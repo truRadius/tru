47be4e259a3a94dbae1e0ef78fe8cddda1bb4cf1
Arwa <arwa.it15@gmail.com>
2018-11-28 16:07:51 -0500

    adding new package

--- src/stories/layout.tsx
+++ src/stories/layout.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
-import { OrganizationProfile } from '../modules/profile/OrganizationProfile';
+import { OrganizationProfile } from '../modules/Profile/OrganizationProfile';
 
 storiesOf('Profile Pages', module)
 .add('Organization Profile Page', () => {
