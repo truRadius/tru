6885681ffa9042bfa2ee6c40f1331b0f3feaf5ee
Arwa <arwa.it15@gmail.com>
2018-11-29 10:04:16 -0500

    use token to get current user's data

--- backend/controllers/ExternalApi-Ctrl.js
+++ backend/controllers/ExternalApi-Ctrl.js
@@ -1,35 +1,38 @@
 'use strict';
 let axios = require('axios');
 
-// const { dbGetOneAccount } = require('../models/Account');
+const { dbGetOneAccount } = require('../models/Account');
+
 let { apiKey } = require('../config.json');
 module.exports.getDataFromExternalApi = (req, res, next) => {
   let term = req.body.search_text;
   let dist = req.body.distance;
-  console.log('------------->', req.body);
-  axios
-    .post(
-      'https://apidata.guidestar.org/essentials/v1',
-      {
-        search_terms: term,
-        filters: {
-          geography: {
-            radius: dist,
-            zip: '33647' //TODO: watch the video. Use token to get user's id and get their zip code from it.
+  dbGetOneAccount().then(data => {
+    console.log('------------->', req.body);
+    axios
+      .post(
+        'https://apidata.guidestar.org/essentials/v1',
+        {
+          search_terms: term,
+          filters: {
+            geography: {
+              radius: dist,
+              zip: '33647' //TODO: watch the video. Use token to get user's id and get their zip code from it.
+            }
+          }
+        },
+        {
+          headers: {
+            'Subscription-Key': apiKey,
+            'Content-Type': 'application/json'
           }
         }
-      },
-      {
-        headers: {
-          'Subscription-Key': apiKey,
-          'Content-Type': 'application/json'
-        }
-      }
-    )
-    .then(result => {
-      res.send(result.data.data.hits);
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      )
+      .then(result => {
+        res.send(result.data.data.hits);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
 };
