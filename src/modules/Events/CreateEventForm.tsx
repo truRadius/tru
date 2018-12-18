8c4d1214c70c87d2788d2b744a1e8708b612da4c
Arwa <arwa.it15@gmail.com>
2018-12-18 15:07:01 -0500

    complete create event form without backend attached

--- src/modules/Events/CreateEventForm.tsx
+++ src/modules/Events/CreateEventForm.tsx
@@ -13,10 +13,11 @@ import {
   GridList,
   GridListTile,
   GridListTileBar,
-  // IconButton,
-  Radio
+  Grid,
+  TextField,
+  IconButton
 } from '@material-ui/core';
-// import { StarBorder } from '@material-ui/icons';
+import { StarBorder, StarRate } from '@material-ui/icons';
 import { CSSProperties } from '@material-ui/core/styles/withStyles';
 import art from './art.jpg';
 import art2 from './art2.jpg';
@@ -25,7 +26,8 @@ import art4 from './art4.jpg';
 import art5 from './art5.jpg';
 import art6 from './art6.jpg';
 import art7 from './art7.jpg';
-
+import none from './noimage.jpg';
+let zipcodes = require('zipcodes');
 interface StateProps {}
 
 interface DispatchProps {}
@@ -69,22 +71,62 @@ const styles = (theme: Theme): { [key: string]: CSSProperties } => ({
   },
   carousel: {
     padding: '20px 50px'
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap'
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200
+  },
+  removeImage: {
+    borderRadius: '100%',
+    position: 'fixed',
+    color: 'red',
+    backgroundColor: 'white'
   }
 });
 
 type PropsWithStyles = StateProps &
   DispatchProps &
   WithTheme &
-  WithStyles<'root' | 'formDiv' | 'button' | 'input' | 'selectedImg' | 'title' | 'titleBar' | 'gridList' | 'carousel'>;
+  WithStyles<
+    | 'root'
+    | 'formDiv'
+    | 'button'
+    | 'input'
+    | 'selectedImg'
+    | 'title'
+    | 'titleBar'
+    | 'gridList'
+    | 'carousel'
+    | 'paper'
+    | 'textField'
+    | 'container'
+    | 'removeImage'
+  >;
 
 class CreateEvent extends React.PureComponent<PropsWithStyles, InternalState> {
   state = {
     image: '',
-    file: '',
-    selectedImage: ''
+    selectedImage: '',
+    eventName: '',
+    streetAddress: '',
+    city: '',
+    zipcode: '',
+    state: '',
+    startDateTime: '',
+    endDateTime: '',
+    eventDescription: ''
   };
 
   tileData = [
+    {
+      img: none,
+      title: 'None'
+    },
     {
       img: art,
       title: 'Art1'
@@ -117,65 +159,225 @@ class CreateEvent extends React.PureComponent<PropsWithStyles, InternalState> {
 
   handleChange = (name: string) => (event: any) => {
     if (name === 'image') {
-      let reader = new FileReader();
-      reader.onload = (event: any) => {
-        this.setState({ [name]: event.target.result });
-      };
-      reader.readAsDataURL(event.target.files[0]);
+      if (this.state.selectedImage === '') {
+        let reader = new FileReader();
+        reader.onload = (event: any) => {
+          this.setState({ [name]: event.target.result });
+        };
+        reader.readAsDataURL(event.target.files[0]);
+      } else {
+        alert('You have already selected one from the list');
+      }
     } else {
       this.setState({ [name]: event.target.value });
     }
+  };
+
+  handleImageChange = (event: any) => {
+    if (this.state.image !== '') {
+      alert('You have already uploaded an image');
+    } else this.setState({ selectedImage: event.target.id });
+  };
+
+  handleZipcodeChange = (event: any) => {
+    const onlyNums = event.target.value.replace(/[^0-9]/, '');
+    // tslint:disable-next-line:no-console
+    if (onlyNums.length < 5) {
+      this.setState({ zipcode: onlyNums });
+    } else if (onlyNums.length === 5) {
+      this.setState({ zipcode: onlyNums });
+      this.findCityState(onlyNums);
+    }
+  };
+
+  findCityState = (nums: number) => {
+    if (zipcodes.lookup(nums) !== undefined) {
+      let city = zipcodes.lookup(nums).city;
+      let state = zipcodes.lookup(nums).state;
+      let cs = `${city}, ${state}`;
+      this.setState({ cs: cs, city: city, state: state });
+    } else {
+      alert('Incorrect zipcode! Try again.');
+    }
+  };
+
+  createEventObj = () => {};
+
+  removeImage = () => {
+    this.setState({ image: '' });
+  };
+
+  onSubmit = (event: any) => {
+    event.preventDefault();
+    console.log('On submit', this.state);
+    //create object
+    this.createEventObj();
+    //send it to database
   };
 
   render() {
     const { classes } = this.props;
     return (
       <div>
-        <div className={classes.formDiv}>Create Event</div>
-        <input
-          accept=".png,.jpg,.jpeg"
-          onChange={this.handleChange('image')}
-          className={classes.input}
-          id="contained-button-file"
-          type="file"
-          multiple
-        />
-        <label htmlFor="contained-button-file">
-          <Button variant="contained" component="span" className={classes.button}>
-            Upload Photo
-          </Button>
-        </label>
-        {this.state.image === '' ? '' : <img src={this.state.image} className={classes.selectedImg} />}
-        <span> - or - </span>
-        <span>Select one from the list</span>
-        <div id="img_carousel" className={classes.carousel}>
-          <div className={classes.root}>
-            <GridList className={classes.gridList} cols={2.5}>
-              {this.tileData.map(tile => (
-                <GridListTile key={tile.img}>
-                  <img src={tile.img} alt={tile.title} />
-                  <GridListTileBar
-                    title={tile.title}
-                    classes={{
-                      root: classes.titleBar,
-                      title: classes.title
-                    }}
-                    actionIcon={
-                      <Radio
-                        className={classes.title}
-                        checked={this.state.selectedImage === tile.title}
-                        onChange={this.handleChange('selectedImage')}
-                        value={tile.title}
-                        name="radio-button-demo"
-                        aria-label="A"
+        <form onSubmit={this.onSubmit}>
+          <div className={classes.formDiv}>Create Event</div>
+          <input
+            accept=".png,.jpg,.jpeg"
+            onChange={this.handleChange('image')}
+            className={classes.input}
+            id="contained-button-file"
+            type="file"
+            multiple
+          />
+          <label htmlFor="contained-button-file">
+            <Button variant="contained" component="span" className={classes.button}>
+              Upload Photo
+            </Button>
+          </label>
+          {this.state.image === '' ? (
+            ''
+          ) : (
+            <div>
+              <img src={this.state.image} className={classes.selectedImg} />
+              <button type="delete" className={classes.removeImage} onClick={this.removeImage}>
+                x
+              </button>
+            </div>
+          )}
+          <span> - or - </span>
+          <span>Select one from the list</span>
+          <div id="img_carousel" className={classes.carousel}>
+            <div className={classes.root}>
+              <GridList className={classes.gridList} cols={2.5}>
+                {this.tileData.map(tile => (
+                  <GridListTile key={tile.img}>
+                    <img src={tile.img} alt={tile.title} />
+                    <GridListTileBar
+                      title={tile.title}
+                      classes={{ root: classes.titleBar, title: classes.title }}
+                      actionIcon={
+                        <IconButton id={tile.title} onClick={this.handleImageChange}>
+                          {this.state.selectedImage !== '' && this.state.selectedImage === tile.title ? (
+                            <StarRate className={classes.title} id={tile.title} />
+                          ) : (
+                            <StarBorder className={classes.title} id={tile.title} />
+                          )}
+                        </IconButton>
+                      }
+                    />
+                  </GridListTile>
+                ))}
+              </GridList>
+              <form>
+                <Grid container spacing={16}>
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      required
+                      id="standard-fname"
+                      label="Event Name"
+                      value={this.state.eventName}
+                      onChange={this.handleChange('eventName')}
+                      margin="normal"
+                    />
+                  </Grid>
+                  <Grid item xs={4}>
+                    <TextField
+                      fullWidth
+                      required
+                      id="address"
+                      label="Street Address"
+                      value={this.state.streetAddress}
+                      onChange={this.handleChange('streetAddress')}
+                      margin="normal"
+                    />
+                  </Grid>
+                  <Grid item xs={2}>
+                    <TextField
+                      fullWidth
+                      required
+                      id="standard-zipcode"
+                      label="Zipcode"
+                      value={this.state.zipcode}
+                      onChange={this.handleZipcodeChange}
+                      margin="normal"
+                    />
+                  </Grid>
+                  <Grid item xs={2}>
+                    <TextField
+                      fullWidth
+                      required
+                      id="city"
+                      label="City"
+                      value={this.state.city}
+                      onChange={this.handleChange('city')}
+                      margin="normal"
+                    />
+                  </Grid>
+                  <Grid item xs={2}>
+                    <TextField
+                      fullWidth
+                      required
+                      id="state"
+                      label="State"
+                      value={this.state.state}
+                      onChange={this.handleChange('state')}
+                      margin="normal"
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <form className={classes.container} noValidate>
+                      <TextField
+                        required
+                        id="start-datetime"
+                        label="Start Date & Time"
+                        type="datetime-local"
+                        className={classes.textField}
+                        value={this.state.startDateTime}
+                        InputLabelProps={{
+                          shrink: true
+                        }}
+                        onChange={this.handleChange('startDateTime')}
                       />
-                    }
-                  />
-                </GridListTile>
-              ))}
-            </GridList>
+                    </form>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <form className={classes.container} noValidate>
+                      <TextField
+                        required
+                        id="end-datetime"
+                        label="End Date & Time"
+                        type="datetime-local"
+                        value={this.state.endDateTime}
+                        className={classes.textField}
+                        InputLabelProps={{
+                          shrink: true
+                        }}
+                        onChange={this.handleChange('endDateTime')}
+                      />
+                    </form>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      fullWidth
+                      required
+                      id="description"
+                      label="Event Description"
+                      value={this.state.eventDescription}
+                      onChange={this.handleChange('eventDescription')}
+                      margin="normal"
+                    />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Button color="primary" className={classes.button} type="submit">
+                      <span style={{ color: 'white' }}>Submit</span>
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     );
   }
